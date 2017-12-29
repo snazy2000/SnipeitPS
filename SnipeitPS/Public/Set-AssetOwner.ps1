cf23662a4279716e21222368d53ba9619f93d1b9
Stephen Maunder <stephen.maunder@datainterchange.com>
2017-12-29 14:06:27 +0000

    Changes to fix changes in newer build

--- SnipeitPS/Public/Set-AssetOwner.ps1
+++ SnipeitPS/Public/Set-AssetOwner.ps1
@@ -10,7 +10,10 @@ function Set-AssetOwner()
         [int]$id,
 
         [parameter(mandatory = $true)]
-        [int]$user_id,
+        [int]$assigned_id,
+
+        [ValidateSet("location","asset","user")]
+        [string] $checkout_to_type = "user",
 
         [parameter(mandatory = $true)]
         [string]$url,
@@ -20,7 +23,15 @@ function Set-AssetOwner()
     )
 
     $Values = @{
-        "user_id" = $user_id
+        "id" = $assigned_id
+        "checkout_to_type" = $checkout_to_type
+    }
+
+    switch ($checkout_to_type)
+    {
+        'location' { $Values += @{ "assigned_location" = $assigned_id } }
+        'user' { $Values += @{ "assigned_user" = $assigned_id } }
+        'asset' { $Values += @{ "assigned_asset" = $assigned_id } }
     }
 
     $Body = $Values | ConvertTo-Json;
